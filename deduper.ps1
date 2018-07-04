135a9b3bf6b522f7a3fc1191392d4d2661512fec
Magister Machinis <30908581+Magister-Machinis@users.noreply.github.com>
2018-07-03 22:46:19 -0400

    initial prototype

---
+++ deduper.ps1
@@ -0,0 +1,41 @@
+param(
+	[string]$targetfile=".\target.csv",
+	[string]$outputfile = ".\output.csv"
+)
+
+foreach($item in $MyParam.Keys)
+{
+	New-Item (Get-Variable $item).Value -ItemType File -ErrorAction SilentlyContinue
+	(Get-Variable $item).Value = Resolve-Path (Get-Variable $item).Value 
+	Write-Host "Resolving $((Get-Variable $item).Value)"
+}
+$indexkey= 5
+$target = Import-Csv $targetfile
+
+$intermediate = @{}
+
+foreach($line in $target)
+{
+	$keytest = $line.$($line.keys[$indexkey])
+	if($intermediate.keys.contains($keytest))
+	{
+		foreach($key in $line.keys)
+		{
+			if($line.$key -eq "x" -or $line.$key -eq "X")
+			{
+				$intermediate.$keytest.$key = $line.$key
+			}
+		}
+	}
+	else
+	{
+		$intermediate.add($keytest, $line)
+	}	
+}
+$output = @()
+foreach($item in $intermediate)
+{
+	$output += $item
+}
+
+$output | export-csv $outputfile
